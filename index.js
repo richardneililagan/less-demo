4044fd44770a789432eb56542f9bc6ff1fa45f00
Richard Neil Ilagan <richard.ilagan@hp.com>
2014-03-01 01:58:33 +0800

    Touchpoint.

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express'),
     app = express()
     ;
 
-app.use('/', express.static(__dirname + 'pub/'));
+app.use('/a', express.static(__dirname + '/pub'));
+
+app.get('/', function (req, res) {
+    res.sendfile('./pub/index.html');
+});
 
 app.listen(8080, function () {
     console.log('App listening at port 8080.');
